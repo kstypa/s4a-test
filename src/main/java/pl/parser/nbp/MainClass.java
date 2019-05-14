986a35282738f227337b2562f1bb1de1b4a91d2c
Karol Stypa <bury09@gmail.com>
2019-05-14 04:38:47 +0200

    added preparing URL based on parsed date and validation

--- src/main/java/pl/parser/nbp/MainClass.java
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -1,43 +1,66 @@
 package pl.parser.nbp;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 public class MainClass {
+    private static String currencyCode;
+    private static LocalDate startDate;
+    private static LocalDate endDate;
 
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Podaj poprawne argumenty w postaci: \"KOD YYYY-MM-DD YYYY-MM-DD\"");
-            return;
-        }
-
-        String currencyCode = args[0];
-        String startDate = args[1];
-        String endDate = args[2];
-
-        // validate input
+        validateInput(args);
 
         Parser parser = new Parser(currencyCode);
-        List<Currency> rates = parser.getRates(startDate, endDate);
+        parser.downloadRates(startDate, endDate);
 
-        ArrayList<Double> buyRates = new ArrayList<Double>();
-        ArrayList<Double> sellRates = new ArrayList<Double>();
-
-        for (Currency currency : rates) {
-            buyRates.add(currency.getBuyRate());
-            sellRates.add(currency.getSellRate());
-        }
+        List<Double> buyRates = parser.getBuyRates();
+        List<Double> sellRates = parser.getSellRates();
 
         double buyMean = Statistics.calculateMean(buyRates);
         double sellStdDev = Statistics.calculateStdDev(sellRates);
 
         DecimalFormat decimalFormat = new DecimalFormat("###0.0000");
 
-        System.out.println("Waluta: " + currencyCode);
+        System.out.println("Waluta: " + parser.getMultiplier() + " " + currencyCode);
         System.out.println("Data: " + startDate + " - " + endDate);
-        System.out.println("Średni kurs kupna: " + decimalFormat.format(buyMean));
-        System.out.println("Odchylenie standardowe kursów sprzedaży: " + decimalFormat.format(sellStdDev));
+        if (buyRates.size() == 0 || sellRates.size() == 0) {
+            System.out.println("Brak danych dla podanego zakresu dat");
+        }
+        else {
+            System.out.println("Średni kurs kupna: " + decimalFormat.format(buyMean));
+            System.out.println("Odchylenie standardowe kursów sprzedaży: " + decimalFormat.format(sellStdDev));
+        }
+    }
 
+    private static void validateInput(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Podaj poprawne argumenty w postaci: \"KOD YYYY-MM-DD YYYY-MM-DD\"");
+            System.exit(-1);
+        }
+
+        currencyCode = args[0].toUpperCase();
+        String startDateText = args[1];
+        String endDateText = args[2];
+
+        if (currencyCode.length() != 3) {
+            System.out.println("Kod waluty nie jest poprawny");
+            System.exit(-1);
+        }
+
+        try {
+            startDate = LocalDate.parse(startDateText);
+            endDate = LocalDate.parse(endDateText);
+
+            if (endDate.isBefore(startDate) || startDate.getYear() < 2002 || endDate.isAfter(LocalDate.now())) {
+                System.out.println("Podany zakres dat jest niepoprawny. Podaj datę z zakresu 2002-01-01 do " + LocalDate.now());
+                System.exit(-1);
+            }
+        } catch (DateTimeParseException e) {
+            System.out.println("Podana data jest niepoprawna");
+            System.exit(-1);
+        }
     }
 }
--- src/main/java/pl/parser/nbp/Parser.java
+++ src/main/java/pl/parser/nbp/Parser.java
@@ -7,37 +7,54 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
     private Document document;
-    private List<Currency> rates;
     private String currencyCode;
+    private int multiplier = 1;
+    private List<Double> buyRates;
+    private List<Double> sellRates;
 
     public Parser(String code) {
         currencyCode = code;
-        rates = new ArrayList<Currency>();
+        buyRates = new ArrayList<>();
+        sellRates = new ArrayList<>();
     }
 
-    private void downloadAndParseXML(String date) {
+    public int getMultiplier() {
+        return multiplier;
+    }
+
+    public List<Double> getBuyRates() {
+        return buyRates;
+    }
+
+    public List<Double> getSellRates() {
+        return sellRates;
+    }
+
+    private void downloadAndParseXML(URL url) {
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
 
-            URL url = new URL("http://www.nbp.pl/kursy/xml/c022z130131.xml");
             InputStream inputStream = url.openStream();
             Document document = builder.parse(inputStream);
             document.getDocumentElement().normalize();
 
             this.document = document;
 
-        } catch (MalformedURLException e) {
-            System.out.println("Adres URL nie jest poprawny");
         } catch (IOException e) {
             System.out.println("Nie można otworzyć pliku XML");
         } catch (Exception e) {
@@ -55,28 +72,72 @@ public class Parser {
                 String code = element.getElementsByTagName("kod_waluty").item(0).getTextContent();
 
                 if (code.equals(currencyCode)) {
-                    String name = element.getElementsByTagName("nazwa_waluty").item(0).getTextContent();
-                    int multiplier = Integer.parseInt(element.getElementsByTagName("przelicznik").item(0).getTextContent());
+                    multiplier = Integer.parseInt(element.getElementsByTagName("przelicznik").item(0).getTextContent());
 
                     String buyRateText = element.getElementsByTagName("kurs_kupna").item(0).getTextContent().replace(",", ".");
                     double buyRate = Double.parseDouble(buyRateText);
                     String sellRateText = element.getElementsByTagName("kurs_sprzedazy").item(0).getTextContent().replace(",", ".");
                     double sellRate = Double.parseDouble(sellRateText);
 
-                    Currency newRate = new Currency(name, multiplier, code, buyRate, sellRate);
-                    rates.add(newRate);
+                    buyRates.add(buyRate);
+                    sellRates.add(sellRate);
                 }
             }
         }
     }
 
-    public List<Currency> getRates(String startDate, String endDate) {
-        // prepare list of dates
-        // for {
-        downloadAndParseXML("");
-        addRate();
-        // }
+    public void downloadRates(LocalDate startDate, LocalDate endDate) {
+        Period period = startDate.until(endDate);
+        int days = period.getDays() + 1;
 
-        return rates;
+        for (int i = 0; i < days; ++i) {
+            LocalDate date = startDate.plusDays(i);
+            URL url = prepareURL(date);
+            if (url == null) {
+                System.out.println("Brak danych dla dnia " + date);
+                continue;
+            }
+            downloadAndParseXML(prepareURL(date));
+            addRate();
+        }
+    }
+
+    private URL prepareURL(LocalDate date) {
+        try {
+            DecimalFormat decimalFormat = new DecimalFormat("00");
+            String yearString = Integer.toString(date.getYear()).substring(2, 4);
+            String monthString = decimalFormat.format(date.getMonthValue());
+            String dayString = decimalFormat.format(date.getDayOfMonth());
+            String dateString = yearString + monthString + dayString;
+
+            int currentYear = LocalDate.now().getYear();
+            String year;
+            if (date.getYear() == currentYear)
+                year = "";
+            else
+                year = Integer.toString(date.getYear());
+
+            URL list = new URL("https://www.nbp.pl/kursy/xml/dir" + year + ".txt");
+            InputStreamReader inputStream = new InputStreamReader(list.openStream());
+            BufferedReader reader = new BufferedReader(inputStream);
+
+            String filename;
+            while ((filename = reader.readLine()) != null) {
+                if (filename.charAt(0) == 'c' && filename.contains(dateString))
+                    break;
+            }
+
+            reader.close();
+
+            if (filename == null)
+                return null;
+
+            return new URL("https://www.nbp.pl/kursy/xml/" + filename + ".xml");
+        } catch (MalformedURLException e) {
+            System.out.println("Adres URL nie jest poprawny");
+        } catch (IOException e) {
+            System.out.println("Nie można otworzyć listy plików XML");
+        }
+        return null;
     }
 }
